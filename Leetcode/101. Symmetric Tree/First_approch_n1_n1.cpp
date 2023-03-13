0c6c690db33cf79aa3a6a58cef25ab797ca3ef19
Japanjotsingh <109128579+Japanjotsingh@users.noreply.github.com>
2023-03-13 08:15:00 +0530

    13march letcode

---
+++ Leetcode/101. Symmetric Tree/First_approch_n1_n1.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool helper(TreeNode* a , TreeNode* b )
+    {   if( a ==  NULL  && b == NULL)
+          return true;
+        
+        if( a ==NULL  || b == NULL)
+            return false;
+     
+     
+        
+        if(a->val !=  b->val )
+            return false;
+      
+        return helper(a->left,b->right) && helper(a->right , b->left);  
+    }    
+    
+    bool isSymmetric(TreeNode* root) 
+    {  
+        
+       return helper(root , root); 
+    }
+};
